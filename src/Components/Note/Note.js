1660b690df9b9833f28b3910bed65ad9627c3df7
Jagrath <jagrathshettylxix@gmail.com>
2022-01-18 20:24:01 +0530

    Fixed date UI

--- src/Components/Note/Note.js
+++ src/Components/Note/Note.js
@@ -2,6 +2,39 @@ import React from "react";
 import "./Note.css";
 import deleteIcon from "../../images/binIcon.png";
 const Note = (props) => {
+  const formatDate = (value) => {
+    if (!value) return "";
+
+    const date = new Date(value);
+    const monthNames = [
+      "Jan",
+      "Feb",
+      "March",
+      "April",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sept",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+
+    let hrs = date.getHours();
+    let amPm = hrs >= 12 ? "PM" : "AM";
+    hrs = hrs ? hrs : "12";
+    hrs = hrs > 12 ? (hrs = 24 - hrs) : hrs;
+
+    let min = date.getMinutes();
+    min = min < 10 ? "0" + min : min;
+
+    let day = date.getDate();
+    const month = monthNames[date.getMonth()];
+
+    return `${hrs}:${min} ${amPm} ${day} ${month}`;
+  };
+
   return (
     <div className="note " style={{ backgroundColor: props.note.color }}>
       <textarea
@@ -9,7 +42,7 @@ const Note = (props) => {
         defaultValue={props.note.text}
       />
       <div className="note_footer">
-        <p>{props.note.time} </p>
+        <p>{formatDate(props.note.time)} </p>
         <img
           className="delete-icon"
           src={deleteIcon}
