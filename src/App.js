9266f1b9e4847f99d37b74276e8687fb62256408
Jagrath <jagrathshettylxix@gmail.com>
2022-01-18 11:39:13 +0530

    Minor adjustments

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NoteContainer from "./Components/NoteContainer/NoteContainer";
 import "./App.css";
+import Sidebar from "./Components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
+      <Sidebar />
       <NoteContainer />
     </div>
   );
